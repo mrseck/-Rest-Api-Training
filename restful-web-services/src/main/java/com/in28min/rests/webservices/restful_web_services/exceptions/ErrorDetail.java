19a55c01e020bc671733c352e8899d348164840d
Ismael Seck <ismaelseck11@gmail.com>
2024-09-27 15:28:50 +0000

    Customized responde handling

---
+++ restful-web-services/src/main/java/com/in28min/rests/webservices/restful_web_services/exceptions/ErrorDetail.java
@@ -0,0 +1,32 @@
+package com.in28min.rests.webservices.restful_web_services.exceptions;
+
+import java.time.LocalDate;
+
+public class ErrorDetail {
+    private LocalDate timestamp;
+    private String message;
+    private String detail;
+
+    public ErrorDetail(LocalDate timestamp, String message, String detail) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.detail = detail;
+    }
+
+    public LocalDate getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+
+    public String getDetail() {
+        return detail;
+    }
+
+    
+}
+
+
